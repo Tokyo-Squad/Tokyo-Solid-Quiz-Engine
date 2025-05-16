02534af094c16b231777ae5f9cbaa400dfb995db
Yaseen Mohammed <yasien202020@gmail.com>
2025-05-16 10:00:44 +0300

    removed unnecessary code

--- src/main/kotlin/domain/usecase/AnswerQuestionUseCase.kt
+++ src/main/kotlin/domain/usecase/AnswerQuestionUseCase.kt
@@ -4,16 +4,7 @@ import org.example.domain.model.UserAnswer
 import java.util.UUID
 
 class AnswerQuestionUseCase {
-    operator fun invoke(
-        quizId: UUID,
-        questionId: UUID,
-        answer: Any
-    ): UserAnswer {
-        return UserAnswer(
-            quizId = quizId,
-            questionId = questionId,
-            selectedAnswer = answer,
-            answers = TODO()
-        )
+    operator fun invoke() {
+        TODO("Implementation")
     }
 }

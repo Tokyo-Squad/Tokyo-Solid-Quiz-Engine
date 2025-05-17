5d154a7f322bd6d8073e60334c1280812a907b85
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-05-17 21:54:48 +0300

    fix data exception

--- src/main/kotlin/data/utils/DataException.kt
+++ src/main/kotlin/data/utils/DataException.kt
@@ -6,6 +6,4 @@ class NotFound(message: String) : DataException(message)
 
 class StorageError(message: String, cause: Exception? = null) : DataException(message, cause)
 
-class ValidationError(message: String) : DataException(message)
-
 class Unknown(message: String, cause: Exception? = null) : DataException(message, cause)

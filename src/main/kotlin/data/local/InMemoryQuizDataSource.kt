9ee00e1ac67473d0c67a009c7283d4daf10c81f3
Yaseen Mohammed <yasien202020@gmail.com>
2025-05-17 19:14:32 +0300

    remove unused methods from QuizDataSource interface

--- src/main/kotlin/data/local/InMemoryQuizDataSource.kt
+++ src/main/kotlin/data/local/InMemoryQuizDataSource.kt
@@ -5,19 +5,11 @@ import org.example.domain.model.Quiz
 
 class InMemoryQuizDataSource(): QuizDataSource {
 
-    override fun getAllQuizzes(): List<Quiz> {
-        TODO("Not yet implemented")
-    }
-
     override fun getQuizById(quizId: String): Quiz? {
         TODO("Not yet implemented")
     }
 
     override fun saveQuiz(quiz: Quiz): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteQuiz(quizId: String): Boolean {
         TODO("Not yet implemented")
     }
 
--- src/main/kotlin/data/repository/QuizDataSource.kt
+++ src/main/kotlin/data/repository/QuizDataSource.kt
@@ -3,8 +3,6 @@ package org.example.data.repository
 import org.example.domain.model.Quiz
 
 interface QuizDataSource {
-    fun getAllQuizzes(): List<Quiz>
     fun getQuizById(quizId: String): Quiz?
     fun saveQuiz(quiz: Quiz): String
-    fun deleteQuiz(quizId: String): Boolean
 }

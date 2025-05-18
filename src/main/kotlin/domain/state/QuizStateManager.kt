de2df9d5f2e4b51f9e9f32663bb2be10cc8aedcf
Mina Rizkalla Azmy <52071472+mena-rizkalla@users.noreply.github.com>
2025-05-18 17:08:25 +0300

    Feature/submit quiz (#9)
    * add unit test for SubmitQuizUseCase
    
    * implement SubmitQuizUseCase
    
    * refactor SubmitQuizUseCase
    
    * add exceptions
    
    * add score system
    
    * refactor SubmitQuizUseCase
    
    * implement unit test for DefaultQuizScorer
    
    * Introduce NoQuizStartedException
    
    * remove default message
    
    * use UserAnswer directly for score calculation
    
    * simplify quiz scoring logic

--- src/main/kotlin/domain/state/QuizStateManager.kt
+++ src/main/kotlin/domain/state/QuizStateManager.kt
@@ -1,11 +1,12 @@
 package org.example.domain.state
 
+import org.example.domain.model.UserAnswer
 import java.util.UUID
 
 interface QuizStateManager {
     fun setCurrentQuiz(quizId: UUID)
     fun getCurrentQuizId(): UUID?
     fun addAnswer(questionId: UUID, answer: Any)
-    fun getAnswers(): Map<UUID, Any>
+    fun getAnswers(): UserAnswer
     fun clearState()
 }
--- src/main/kotlin/domain/usecase/SubmitQuizUseCase.kt
+++ src/main/kotlin/domain/usecase/SubmitQuizUseCase.kt
@@ -1,11 +1,47 @@
 package org.example.domain.usecase
 
+import org.example.domain.model.Answer
+import org.example.domain.model.MultipleChoiceQuestion
 import org.example.domain.model.QuizReport
+import org.example.domain.model.TrueFalseQuestion
 import org.example.domain.model.UserAnswer
 import org.example.domain.repository.QuizRepository
+import org.example.domain.scorer.QuizScorer
+import org.example.domain.state.QuizStateManager
+import org.example.domain.utils.NoQuizStartedException
+import org.example.domain.utils.QuizNotFound
+import org.example.domain.utils.QuizUnknownError
+import org.example.domain.utils.QuizValidationFailed
 
-class SubmitQuizUseCase(private val repository: QuizRepository) {
-    operator fun invoke(userAnswer: UserAnswer): QuizReport {
-        TODO("implementation")
+class SubmitQuizUseCase(
+    private val repository: QuizRepository,
+    private val quizStateManger : QuizStateManager,
+    private val quizScorer: QuizScorer
+) {
+    operator fun invoke(): QuizReport {
+
+        val quizId = quizStateManger.getCurrentQuizId() ?: throw NoQuizStartedException("No quiz has been started")
+        val quiz =  try {
+            repository.getQuizById(quizId)
+        } catch (e: Exception) {
+            throw QuizUnknownError("Error fetching quiz", e)
+        }
+        val answers = quizStateManger.getAnswers()
+
+        val correctAnswersCount = quizScorer.calculateScore(quiz, answers)
+
+        val totalQuestions = quiz.questions.size
+        val incorrectAnswers = totalQuestions - correctAnswersCount
+        val finalScore = correctAnswersCount
+
+        quizStateManger.clearState()
+
+        return QuizReport(
+            quizTitle = quiz.title,
+            totalQuestions = totalQuestions,
+            correctAnswers = correctAnswersCount,
+            incorrectAnswers = incorrectAnswers,
+            finalScore = finalScore
+        )
     }
 }

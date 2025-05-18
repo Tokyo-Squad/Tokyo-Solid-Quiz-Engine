4a2b086eff3e3630bc748b238833644832d3b24b
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2025-05-18 15:42:18 +0300

    add InMemoryQuizStateManager.kt

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -1,7 +1,9 @@
 package org.example.di
 
+import org.example.domain.state.InMemoryQuizStateManager
+import org.example.domain.state.QuizStateManager
 import org.koin.dsl.module
 
 val appModule = module {
-
+    single<QuizStateManager> { InMemoryQuizStateManager() }
 }
